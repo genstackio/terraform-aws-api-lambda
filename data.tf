15e7133c40e5b03e7d88a76fd1cb6617eb2291c1
Olivier Hoareau <oha@greenberets.io>
2021-11-10 17:34:17 +0100

    fix: fix

--- data.tf
+++ data.tf
@@ -9,65 +9,21 @@ data "aws_cloudfront_cache_policy" "managed_caching_optimized" {
 }
 
 data "aws_iam_policy_document" "s3_website_policy" {
-  for_each = toset(vars.static_assets)
+  for_each = toset(var.static_assets)
   statement {
     actions   = ["s3:GetObject"]
-    resources = ["${aws_s3_bucket.statics.arn}/*"]
+    resources = ["${aws_s3_bucket.static_assets[each.key].arn}/*"]
     principals {
       type        = "AWS"
-      identifiers = [var.cloudfront_iam_arn]
+      identifiers = [aws_cloudfront_origin_access_identity.oai.iam_arn]
     }
   }
   statement {
     actions   = ["s3:ListBucket"]
-    resources = [aws_s3_bucket.statics.arn]
+    resources = [aws_s3_bucket.static_assets[each.key].arn]
     principals {
       type        = "AWS"
-      identifiers = [var.cloudfront_iam_arn]
+      identifiers = [aws_cloudfront_origin_access_identity.oai.iam_arn]
     }
-  }
-}
-
-data "aws_iam_policy_document" "s3_replication_assume_role_policy" {
-  count = local.replication_master_count
-  statement {
-    actions = ["sts:AssumeRole"]
-    principals {
-      type        = "Service"
-      identifiers = ["s3.amazonaws.com"]
-    }
-  }
-}
-data "aws_iam_policy_document" "s3_replication_policy" {
-  count = local.replication_master_count
-  statement {
-    actions = [
-      "s3:GetReplicationConfiguration",
-      "s3:ListBucket"
-    ]
-    resources = [
-      aws_s3_bucket.statics.arn
-    ]
-    effect = "Allow"
-  }
-  statement {
-    actions = [
-      "s3:GetObjectVersionForReplication",
-      "s3:GetObjectVersionAcl",
-      "s3:GetObjectVersionTagging"
-    ]
-    resources = [
-      "${aws_s3_bucket.statics.arn}/*"
-    ]
-    effect = "Allow"
-  }
-  statement {
-    actions = [
-      "s3:ReplicateObject",
-      "s3:ReplicateDelete",
-      "s3:ReplicateTags"
-    ]
-    resources = [for r in var.replications: "${r}/*"]
-    effect = "Allow"
   }
 }
