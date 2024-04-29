3485465db0869d335e21464722864bfee1a051f3
Olivier Hoareau <oha@greenberets.io>
2024-04-30 00:39:29 +0200

    fix: fix

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 module "api" {
   source            = "genstackio/apigateway2-api/aws"
-  version           = "0.4.3"
+  version           = "0.4.4"
   name              = (null == var.api_name) ? "${var.env}-api-${var.name}" : var.api_name
   lambda_arn        = var.lambda_arn
   lambda_invoke_arn = var.lambda_invoke_arn
